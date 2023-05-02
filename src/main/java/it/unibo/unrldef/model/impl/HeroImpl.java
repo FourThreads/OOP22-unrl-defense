d38ff1b4e6701068896665bbb9191c6a1cdaa580
Tommaso Ceredi <cere.cere_2001@libero.it>
2023-05-02 17:28:05 +0200

    miss file save

--- src/main/java/it/unibo/unrldef/model/impl/HeroImpl.java
+++ src/main/java/it/unibo/unrldef/model/impl/HeroImpl.java
@@ -9,6 +9,7 @@ import it.unibo.unrldef.model.api.Hero;
 public abstract class HeroImpl extends DefenseEntity implements Hero {
 
     private final int cost;
+    private boolean active;
     private Optional<Enemy> target = Optional.empty();
 
     public HeroImpl(final String name, final double radius, final double damage, final long attackRate, final int cost) {
@@ -32,6 +33,11 @@ public abstract class HeroImpl extends DefenseEntity implements Hero {
     @Override
     protected void attack() {
         
+    }
+
+    @Override
+    public final boolean isActive() {
+        return this.active;
     }
     
 }
